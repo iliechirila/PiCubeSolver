b3aa2530fd8ffe683ed3d071f6bfcde50aa0343f
Chirila Ilie <ilieraduchirila@gmail.com>
2022-11-25 23:05:02 +0200

    first commit

---
+++ Codebase/MainApp.py
@@ -0,0 +1,12 @@
+"""
+This is the main file where everything is supposed to run from at this point in time.
+"""
+
+class MainApp:
+    cube = []
+    
+
+
+if __name__ == '__main__':
+    mainApp = MainApp()
+    print(mainApp)
