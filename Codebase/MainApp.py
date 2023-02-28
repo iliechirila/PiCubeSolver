14c08b7f5688ce647c728be85440c16afb72233d
Ilie_Chirila <ilieraduchirila@gmail.com>
2023-02-28 16:53:15 +0200

    Proper comments for cross solves :D

--- Codebase/MainApp.py
+++ Codebase/MainApp.py
@@ -10,13 +10,13 @@ This is the main file where everything is supposed to run from at this point in 
 
 if __name__ == '__main__':
     # For scramble: L' D2 B' L2 U2 B R2 D2 U2 F2 U2 F2 L' R2 D' L R2 F' R
-    # R B' R D D2 R' D D' ?????????????? cross hello?
+    # cross alg: D' R B D L' D B' D'
 
     # For scramble: D' F2 R U2 L B2 F2 L D2 L2 D2 R B' L B2 R' U' R' D B'
-    # R D2 F2 U R2 D' R' D'
+    # R2 F D2 B2 D R' B2
 
     cube = Cube()
-    cube.apply_alg_std("L' D2 B' L2 U2 B R2 D2 U2 F2 U2 F2 L' R2 D' L R2 F' R")
+    cube.apply_alg_std("D' F2 R U2 L B2 F2 L D2 L2 D2 R B' L B2 R' U' R' D B'")
 
 
     cs = CrossSolver(cube.cube_dict)
