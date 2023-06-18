c3ace7ba385f97abac41f5a202ce4e5634e9d6b3
Ilie Chirila <ilieraduchirila@gmail.com>
2023-06-19 02:56:32 +0300

    test stepper

--- Codebase/TestFiles/TestStepper2.py
+++ Codebase/TestFiles/TestStepper2.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
+#=================== STEPPER 1 =============
+
 DIR = 4
 STEP = 3
 
@@ -13,24 +15,42 @@ GPIO.output(STEP, GPIO.LOW)
 # clockwise rotation
 GPIO.output(DIR, GPIO.LOW)
 
+#=================== STEPPER 2 =============
+DIR2 = 17
+STEP2 = 27
 
-steps = 200
-delay = 0.05
+GPIO.setup(DIR2, GPIO.OUT)
+GPIO.setup(STEP2, GPIO.OUT)
 
-for i in range(steps):
-    print(f"Step {i}")
-    GPIO.output(STEP, GPIO.HIGH)
-    time.sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    time.sleep(delay)
+GPIO.output(STEP2, GPIO.LOW)
 
-# counter clockwise
-GPIO.output(DIR, GPIO.HIGH)
-for i in range(steps):
-    print(f"Step {i}")
-    GPIO.output(STEP, GPIO.HIGH)
-    time.sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    time.sleep(delay)
+# clockwise rotation
+GPIO.output(DIR2, GPIO.LOW)
+
+
+
+def stepper_command(steps=100, delay=0.01, dir_pin=4, step_pin=3, rotation="CW"):
+    rotation_pol = GPIO.LOW
+    if rotation=="CW":
+        rotation_pol = GPIO.LOW
+    elif rotation=="CCW":
+        rotation_pol = GPIO.HIGH
+    
+    GPIO.output(dir_pin, rotation_pol)
+    for i in range(steps):
+        print(f"Step {i}")
+        GPIO.output(STEP, GPIO.HIGH)
+        time.sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        time.sleep(delay)
+
+# stepper_command(steps=10, dir_pin=4, step_pin=3, rotation="CW")
+# stepper_command(steps=10, dir_pin=4, step_pin=3, rotation="CCW")
+
+stepper_command(steps=10, dir_pin=DIR2, step_pin=STEP2, rotation="CW")
+stepper_command(steps=10, dir_pin=DIR2, step_pin=STEP2, rotation="CCW")
+
+# stepper_command(steps=10, dir_pin=17, step_pin=27, rotation="CCW")
+
 
 GPIO.cleanup()
